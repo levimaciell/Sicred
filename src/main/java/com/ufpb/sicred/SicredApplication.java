07543442985d511cbb57681c77b956b52b153e69
levimaciell <levimaciell1221@gmail.com>
2024-10-02 20:06:05 -0300

    initial commit

---
+++ src/main/java/com/ufpb/sicred/SicredApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.sicred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicredApplication.class, args);
+	}
+
+}
